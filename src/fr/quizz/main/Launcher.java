7b9f0cc81db0975531831aa8049b6a29ba55380d
Matthieu Clin <matthieu.clin@outlook.com>
2014-02-28 15:00:35 +0100

    1ere version requete

--- src/fr/quizz/main/Launcher.java
+++ src/fr/quizz/main/Launcher.java
@@ -9,6 +9,9 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		//PlayerModel pM = new PlayerModel();
+		//pM.deletePlayer(new Player(1, "", "",""));
 		PlayerController pC = new PlayerController();
 		pC.login();
 	}
--- src/fr/quizz/model/PlayerModel.java
+++ src/fr/quizz/model/PlayerModel.java
@@ -8,17 +8,22 @@ import java.sql.SQLException;
 import fr.quizz.core.Player;
 
 public class PlayerModel extends Model {
-	private Connection connexion;
-	
 
 	public PlayerModel() {
 		super();
 		
 	}
 
+	
+	/**
+	 * Function which check if a Player is registered in the database
+	 * @param name the name of the Player
+	 * @param password his password
+	 * @return an object Player if it is in the database, null otherwise
+	 */
 	public Player exist(String name, String password)
 	{
-		connexion = super.getConnection();
+		Connection connexion = super.getConnection();
 		ResultSet res = null;
 		String sql = "SELECT * FROM joueur WHERE nom_joueur=? AND passwd_joueur=?";
 		Player player = null;
@@ -43,4 +48,41 @@ public class PlayerModel extends Model {
 		super.closeConnection(connexion);
 		return player;
 	}
+
+    /**
+     * Function which allows to save a Question in database
+     * @param question, the question to save
+     */
+    public void deletePlayer(Player player){
+
+    	if(player.getCode() == -1) return;
+    	
+    	Connection connexion = super.getConnection();
+        
+        PreparedStatement requete = null;
+        try
+        {
+        		connexion.setAutoCommit(false);
+        		String sql = "DELETE FROM joueur WHERE code_joueur = ? ";
+                requete  = connexion.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);    
+                requete.setInt(1, player.getCode());
+                
+                int res = requete.executeUpdate();
+                System.out.println("res 1er:"+res);
+        		sql = "DELETE FROM quizz WHERE code_joueur = ? ";
+                requete  = connexion.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);    
+                requete.setInt(1, player.getCode());
+                res = requete.executeUpdate();
+                System.out.println("res 2e:"+res);
+                super.commit(connexion);
+        }
+        catch(SQLException e)
+        {
+        		super.rollback(connexion);
+                System.err.println("Probleme avec lors de la transaction" + e);
+        }
+        super.closeStatement(requete);
+        super.closeConnection(connexion);
+    }
+
 }
